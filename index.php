4138b6de476c06eb37fd12685b59e4a943f8e69a
triss90405 <tw@d3sign.dk>
2015-04-01 20:23:37 +0200

    Adding vars for layouts

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
         <footer>
             <p>Made with
             <svg class="heart" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="92.515px" height="73.161px" viewBox="0 0 92.515 73.161" enable-background="new 0 0 92.515 73.161" xml:space="preserve"><g><path fill="#010101" d="M82.32,7.888c-8.359-7.671-21.91-7.671-30.271,0l-5.676,5.21l-5.678-5.21c-8.357-7.671-21.91-7.671-30.27,0c-9.404,8.631-9.404,22.624,0,31.255l35.947,32.991L82.32,39.144C91.724,30.512,91.724,16.52,82.32,7.888z"></path></g></svg> 
-            by Tristan White</p>
+            by <a href="https://github.com/triss90405" target="_target">Tristan White</a></p>
             &copy;Copyright <?php echo date("Y") ?> - <a href="http://d3sign.dk/">d3sign</a>
         </footer>
 
